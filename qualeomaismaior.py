812bab6792cf72c76a444258954ec0311e2f1154
Koji-Kashin <daniel.f.rodrigues6@aluno.senai.br>
2026-06-01 21:38:28 -0300

    att...

--- qualeomaismaior.py
+++ qualeomaismaior.py
@@ -6,12 +6,15 @@ print("Boa-noite, vamos descobrir o valor mais maior de todos")
 num1 = int (input("Digite o primeiro número(EM NÚMEROS): "))
 num2 = int (input("Digite o segundo número(EM NÚMEROS TAMBÉM)"))
 
+# Verificações
+
+#Primeira verificação
 if(num1 > num2):
     print("--- O número ", num1, " é maior do que o número ", num2, " ---")
-    
+# Se a de cima der errado tenta essa
 elif(num1 == num2):
     print("--- Os números ", num1, " e ", num2, " são iguais ---")
-    
+# Se todas as outras não forem, chega até aqui    
 else:
     print("--- O número ", num2, " é maior do que o número ", num2, " ---")
        
